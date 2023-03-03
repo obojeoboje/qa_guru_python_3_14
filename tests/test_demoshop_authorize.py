4fddce2ba12823bc73f3a109e69378c99a31c02d
Aleksandr Privalov <obojealexander@gmail.com>
2023-03-03 09:18:12 +0300

    refactor

--- tests/test_demoshop_authorize.py
+++ tests/test_demoshop_authorize.py
@@ -16,7 +16,7 @@ browser.config.base_url = WEB_URL
 
 def test_login_through_api(login):
     with step("Verify successful authorization"):
-        browser.element(".account").should(have.text(LOGIN))
+        browser.all(".account").first.should(have.text(os.getenv('LOGIN'))).click()
 
 def test_search_negative_result(login):
     with allure.step("Negitive search"):
@@ -24,25 +24,24 @@ def test_search_negative_result(login):
         browser.element('.search-box [value="Search store"]').type('negative test').press_enter()
         browser.element('.result').should(have.text('No products were found that matched your criteria.'))
 
-def test_logout(login):
-    with allure.step("Check logout"):
-        browser.element('.ico-logout').click()
-        browser.element('.ico-login').should(have.text('Log in'))
-
-
 def test_watch_profile(login):
     with allure.step("Check info in profile"):
-        browser.element(".account").should(have.text(os.getenv('LOGIN'))).click()
+        browser.all(".account").first.should(have.text(os.getenv('LOGIN'))).click()
+        # browser.element(".account").should(have.text(os.getenv('LOGIN'))).click()
         browser.element('#FirstName').should(have.value('Alexander'))
         browser.element('#LastName').should(have.value('Oboje'))
         browser.element('#Email').should(have.value(os.getenv('LOGIN')))
         browser.element('[checked="checked"]#gender-male')
 
-
 def test_watch_page_change_password(login):
     with allure.step("Check text buttion in change password"):
-        browser.element(".account").should(have.text(os.getenv('LOGIN'))).click()
+        browser.all(".account").first.should(have.text(os.getenv('LOGIN'))).click()
         browser.element('[href="/customer/changepassword"]').should(have.text('Change password')).click()
         browser.element('[for="OldPassword"]').should(have.text('Old password:'))
         browser.element('[for="NewPassword"]').should(have.text('New password:'))
         browser.element('[for="ConfirmNewPassword"]').should(have.text('Confirm password:'))
+
+def test_logout(login):
+    with allure.step("Check logout"):
+        browser.element('.ico-logout').click()
+        browser.element('.ico-login').should(have.text('Log in'))
